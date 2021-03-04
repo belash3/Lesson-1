51fbcb25769b6373192ddad1b86e52b58511334f
Sergey <belash3@mail.ru>
2021-03-04 20:43:04 +0300

    First part of lesson 1

--- Lesson 1/main.swift
+++ Lesson 1/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
-print("Hello, World!")
+let equation = "x^2-11x+18=0"
+let a: Double  = 1
+let b: Double  = -11
+let c: Double  = 18
+
+let discriminant: Double  = b*b - 4*a*c
+let x1: Double = ((b) * (-1) + (sqrt(discriminant))) / (2*a)
+let x2: Double = ((b) * (-1) - (sqrt(discriminant))) / (2*a)
+    print ("Уравнение ", equation, "имеет 2 корня :", x1, ", ", x2)
 
