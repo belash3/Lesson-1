5dc9ca5eead1e7d47ccf7ea254ac832d9d0d34f3
Sergey <belash3@mail.ru>
2021-03-04 23:50:07 +0300

    Homework lesson 1 Sergey Belyakov

--- Lesson 1/main.swift
+++ Lesson 1/main.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// Задание 1: решить квадратное уравнение.
+
 let equation = "x^2-11x+18=0"
 let a: Double  = 1
 let b: Double  = -11
@@ -17,4 +19,19 @@ let x1: Double = ((b) * (-1) + (sqrt(discriminant))) / (2*a)
 let x2: Double = ((b) * (-1) - (sqrt(discriminant))) / (2*a)
     print ("Уравнение ", equation, "имеет 2 корня :", x1, ", ", x2)
 
-//hhg
+// Задание 2: Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
+
+let kat1: Double = 4
+let kat2: Double = 7
+let gip: Double = sqrt((kat1 * kat1) + kat2 * kat2)
+let p: Double = kat1 + kat2 + gip
+let s: Double = (kat1 * kat2) / 2
+print("Гипотенуза равна: ", gip)
+print("Периметр треугольника равен: ", p)
+print("Площадь треугольника равна: ", s)
+
+let month: Double = 60
+let percent: Double = 13.5
+let dep: Double = 100000
+let sum: Double = dep + dep * ((percent * (30/365)) / 100) * month
+print("Сумма вклада с капитализацией через 5 лет составит: ", sum)
